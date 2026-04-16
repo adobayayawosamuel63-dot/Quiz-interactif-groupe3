28c4c683611d8ddd61d8b49190376400d6bea250
Raïssa <ganniguiraissa@gmail.com>
2026-04-16 15:44:56 +0200

    ajout du quiz

---
+++ script.js
@@ -0,0 +1,29 @@
+const questions = [
+    { question : "Quelle est la capitale de la France ",
+       answers: ["Paris" , "Lyon" , "Nantes" ],
+       correct: 0
+    },
+     { question : "Quelle est la capitale du Bresil ",
+       answers: ["Rio" , "Bordeaux" , "Brasilia" ],
+        correct: 2
+    },
+     { question : "Quelle est la capitale du Mali ",
+       answers : ["Bamako" , "Dakar" , "Nantes" ],
+        correct: 0
+    },
+     { question : "Quelle est la capitale du Portugal ",
+      answers : ["Paris" , "Barcelone" , "Lisbonne" ],
+        correct: 2
+    },
+     { question : "Quelle est la capitale du Japon ",
+     answers : ["Tokyo" , "Séoul" , "Madrid" ],
+        correct: 0
+    },
+     { question : "Quelle est la capitale de l'Egypte ",
+      answers: ["Paris" , "Caire" , "Arsenal" ],
+        correct: 1
+    }
+
+]
+
+
