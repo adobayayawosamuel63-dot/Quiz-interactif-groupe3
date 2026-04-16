b78ebccf699cc83c9bd198b4ba3feb4ce369be6d
Richard Zlatev <richardzlatev@MacBook-Air-de-salopard.local>
2026-04-16 15:47:18 +0200

    creation des questions du quiz

---
+++ script.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: "Que veut dire BMW ?",
+        answers: ["Berlin Motoren Wagen", "Bayerische Motoren Werke", "Blau Mit Weiss"],
+        correct: 2
+    },
+    {
+        question: "Quelle est la première voiture de la division M ?",
+        answers: ["M1", "M3 E30", "M5 E28"],
+        correct: 1
+    },
+    {
+        question: "Quel est le moteur de la BMW M3 E92 ?",
+        answers: ["V8 atmosphérique", "6 en ligne", "V8 Biturbo"],
+        correct: 1
+    },
+    {
+        question: "Quel est le moteur de la BMW M2 (F87 version de base) ?",
+        answers: ["Le S58", "Le N55", "Le B58"],
+        correct: 2
+    },
+    {
+        question: "Comment s'appelle le système 4 roues motrices chez BMW ?",
+        answers: ["X-Drive", "4Matic", "Quattro"],
+        correct: 1
+    }
+];
